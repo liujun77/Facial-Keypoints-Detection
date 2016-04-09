432831267a94eff500f97024f6e09cc051d2f328
liujun <liujun77@gmail.com>
2016-04-09 10:42:02 -0500

    start stcnn

---
+++ stcnn.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 10:39:37 2016
+
+@author: liujun
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import theano
+import theano.tensor as T
+import lasagne 
